461a4e4860d564130f3343d5efa5daa6050a24c2
SRIRAMS2002 <sriramsrit.2002@gmail.com>
2023-07-04 21:17:27 +0530

    phase 2 started

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,16 @@ import Head from './components/Header';
 import Hero from './components/Hero';
 import Foot from './components/Footer';
 import Cont from './components/Contact';
-import Mode from './components/Model';
 
 const App = () => {
   return (
     <div>
-      <Mode/>
+      
       <Navb />
       <Head/>
       <Hero/>
       <Cont/>
-      <Foot/>
+      
       
     
     </div>
--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -4,7 +4,7 @@ const Model = () => {
   return (
     <div>
       
-      <h1 className='text-green-500 text-3xl '>hello</h1>
+      <h1 className='text-white ml-10 pt-20'>Sriram S</h1>
     </div>
   )
 }
